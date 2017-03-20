87112e25978b2e1dd273e0945b582d046e8325ce
JSlovak <jasmine.slovak@gmail.com>
2017-03-20 12:26:59 -1000

    First test passed.

--- linkedList.js
+++ linkedList.js
@@ -5,4 +5,25 @@
  */
 function linkedListGenerator(){
 
+  function getHead() {
+    for (i = 0, i < list.length, i++){
+      if list[i].data =
+    }
+
+  }
+
+  function getTail(list){
+    consol.log("still building");
+  }
+
+  return {
+    getHead:getHead,
+    getTail: getTail,
+
+
+  }
+
 }
+
+
+
