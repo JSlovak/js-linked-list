f46affb0329f2f1580e40f40d212604dcc7a1834
JSlovak <jasmine.slovak@gmail.com>
2017-03-20 13:42:50 -1000

    Returned Object of Methods, all method fuctions are framed and returned.

--- linkedList.js
+++ linkedList.js
@@ -3,25 +3,50 @@
  * @description  Main Module
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
+
 function linkedListGenerator(){
+  var listOfNodes = {};
+  var node = {};
 
-  function getHead() {
-    for (i = 0, i < list.length, i++){
-      if list[i].data =
+
+  function getHead(){
+    for (i= 0; i< list; i++){
+      if ("something"){
+        var head = " value of something";
+        return head;
+      }
     }
-
   }
 
   function getTail(list){
-    consol.log("still building");
+    console.log("still building");
+  }
+
+  function add(Value){
+
+  }
+
+  function get(Number){
+
+  }
+
+  function remove(Number){
+
+  }
+
+  function insert(Value, Number) {
+
   }
 
   return {
     getHead:getHead,
     getTail: getTail,
+    add: add,
+    get: get,
+    remove: remove,
+    insert: insert
 
-
-  }
+  };
 
 }
 
